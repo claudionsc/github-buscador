9d814b055f205c0749c786b032850c6b951e4fa2
Claudio Nascimento <claudio.alves20112011@hotmail.com>
2023-05-09 14:56:00 -0300

    criando e inserindo componente de erro

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -3,15 +3,24 @@ import Search from '../components/Search'
 import { useState } from 'react'
 import { UserProps } from '../types/user'
 import User from '../components/User'
+import Error from '../components/Error'
 
 
 const Home = () => {
     const [user, setUser] = useState<UserProps | null>(null);
+    const [error, setError] = useState(false)
 
     const loadUser = async (userName: string) => {
+        setError(false);
+        setUser(null);
         const res = await fetch(`https://api.github.com/users/${userName}`);
 
         const data = await res.json();
+
+        if(res.status === 404){
+            setError(true);
+            return;
+        }
 
         const {
             avatar_url,
@@ -36,6 +45,7 @@ const Home = () => {
         <div>
             <Search loadUser={loadUser} />
             {user && <User {...user} /> }
+            {error && <Error />}
         </div>
     )
 }
