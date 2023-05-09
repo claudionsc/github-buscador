eaa7325f64fce4fa67bd731dac3221b600668b1a
Claudio Nascimento <claudio.alves20112011@hotmail.com>
2023-05-09 15:12:26 -0300

    finalizando estilização da tela de busca por usuarios

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,6 +1,7 @@
 import { UserProps } from "../types/user"
 import { MdLocationPin } from 'react-icons/md'
 import { Link } from 'react-router-dom'
+import classes from './User.module.css'
 
 
 const User = ({
@@ -11,24 +12,24 @@ const User = ({
     location
 }: UserProps) => {
     return (
-        <div>
+        <div className={classes.user}>
             <img src={avatar_url} alt={login} />
             <h2>{login}</h2>
            {location && ( 
-           <p>
+           <p className={classes.location}>
                 <MdLocationPin />
                 <span>{location}</span>
             </p>
             )}
-            <div>
+            <div className={classes.stats}>
                 <div>
                     <p>Seguidores:</p>
-                    <p>{followers}</p>
+                    <p className={classes.number}>{followers}</p>
                 </div>
 
                 <div>
                     <p>Seguindo:</p>
-                    <p>{following}</p>
+                    <p className={classes.number}>{following}</p>
                 </div>
             </div>
 
