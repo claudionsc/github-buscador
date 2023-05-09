821aa9a59576b8d48a03ce8a779ae4a28d71e452
Claudio Nascimento <claudio.alves20112011@hotmail.com>
2023-05-09 11:40:12 -0300

    criando tipos complexos e reaproveitáveis

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import Search from '../components/Search'
+import { useState } from 'react'
 
 
 const Home = () => {
+    const [user, setUser] = useState(null);
+    
     return (
         <div>
             <Search />
